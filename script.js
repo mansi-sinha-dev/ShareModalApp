91d6345b0f60cb53c68057e126489da7622692c8
mansi-sinha-dev <mansisinha653@gmail.com>
2023-12-01 11:58:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const modal= document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+
+// open modal function
+const openModal=()=>{
+    console.log('Modal is open');
+    modal.classList.add('active');
+    overlay.classList.add('overlayactive');
+}
+
+const closeModal=()=>{
+    console.log('Modal is close');
+    modal.classList.remove('active');
+    overlay.classList.remove('overlayactive');
+}
